87696b838a8e474a93162d6404594317aa560135
Maciej Tutak <maciek.tut@gmail.com>
2018-07-28 19:24:19 +0200

    #10 added classify command

---
+++ code/cybernews_web/collection/management/commands/classify.py
@@ -0,0 +1,51 @@
+import dill as pickle
+from django.core.management.base import BaseCommand, CommandError
+from main.models import Entry, Tag
+import logging
+
+
+class Command(BaseCommand):
+    help = 'Queries the DB for untagged entries and updates them.'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+
+    def handle(self, *args, **options):
+        labels = Tag.objects.all()
+
+        def identity(arg):
+            return arg
+
+        self.logger.debug('Opening svc_model_pipeline.pickle!')
+        with open('svc_model_pipeline.pickle', 'rb') as f:
+            model_lsvc = pickle.load(f)
+            self.logger.info('model loaded OK.')
+
+            queryset = Entry.objects.all().filter(tagged=False)
+            articles_to_classify = [entry.article.text for entry in queryset]
+
+            try:
+                results = model_lsvc.predict(articles_to_classify)
+            except ValueError:
+                self.logger.info('There are no new articles.')
+                raise CommandError('There are no new articles.')
+
+            # x = [[1, 0, 0], [1, 1, 1], [0, 0, 1]]
+            # a = ['a', 'b', 'c']
+            #
+            # result = [['a'], ['a', 'b', 'c'], ['c']]
+            #
+            # [[a[i] for i, j in enumerate(k) if j == 1] for k in x]
+
+            tags = [[labels[i] for i, j in enumerate(k) if j == 1] for k in results]
+
+            for i, entry in enumerate(queryset):
+                entry.tagged = True
+                for tag in tags[i]:
+                    entry.tags.add(tag)
+                entry.save()
+
+            self.logger.info('classify done')
+
+
