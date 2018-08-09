4c5cd0fbfab01f06f1e92afd691a61ce070dcbba
Maciej Tutak <maciek.tut@gmail.com>
2018-08-09 11:20:35 +0200

    added getter to QueryStore #30

---
+++ code/cybernews_ui/src/icons/IconTag.vue
@@ -0,0 +1,22 @@
+<template>
+  <g>
+    <path
+      d=" M 2.145 8.176 L 10.145 16.176 L 16.145 10.176 L 8.145 2.176 Q 2.434 1.824 2.145 2.176 Q 1.855 2.528 2.145 8.176 Z "
+      fill="#999999"
+      stroke="#999999"
+      vector-effect="non-scaling-stroke"
+      stroke-width="1"
+      stroke-linejoin="round"
+      stroke-linecap="square"
+      stroke-miterlimit="3"/>
+    <path
+      d=" M 3 4.5 C 3 3.672 3.672 3 4.5 3 C 5.328 3 6 3.672 6 4.5 C 6 5.328 5.328 6 4.5 6 C 3.672 6 3 5.328 3 4.5 Z "
+      fill="#fafafa" />
+  </g>
+</template>
+
+<script>
+    export default {
+        name: 'IconTag'
+    }
+</script>
--- code/cybernews_ui/src/stores/QueryStore.js
+++ code/cybernews_ui/src/stores/QueryStore.js
@@ -2,7 +2,11 @@ const QueryStore = {
   data: {
     query: 'http://localhost:8000/api/entries/'
   },
+
   methods: {
+    getQuery() {
+      return QueryStore.data.query;
+    },
     setQuery(query) {
       console.log('setQuery triggered');
       QueryStore.data.query = query;
